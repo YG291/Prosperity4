66d3bc70f4ad2fb49530de118654eaeff962c977
YG291 <yuehengguan734@gmail.com>
2026-03-27 22:57:47 -0400

    tomato

--- trader.py
+++ trader.py
@@ -30,7 +30,6 @@ class Trader:
         buy_component = 0
         sell_component = 0
         total = abs(sum(buys.values())) + abs(sum(sells.values()))
-        ratiobuysell = self.buy_sell_ratio(state, product)
         for bprice in buys:
             buy_component += abs(bprice * (buys[bprice]/total))
         for sprice in sells:
@@ -50,6 +49,43 @@ class Trader:
             return bsort[0][0]
         return (bsort[0][0]+ssort[0][0])/2
 
+    def trade_tomatoes(self, state: TradingState, storage, result, order_depth,
+                       buysorted, sellsorted, product = 'TOMATOES'):
+        """
+        Precondition:
+        - product is TOMATOES
+        - if len(storage[product]) < 2
+        """
+        if len(storage[product]) > 50:
+            storage[product].pop(next(iter(storage[product])))
+        intercept, slope = self.regression(product, storage)
+        prediction = slope * int(state.timestamp) + intercept
+
+        orders: List[Order] = []  # Order(symbol, price, quantity)
+        acceptable_price = prediction  # Participant should calculate this value
+
+        buy_buffer = 1
+        if len(order_depth.sell_orders) != 0:
+            for index in range(len(buysorted)):
+                best_ask, best_ask_amount = buysorted[index][0], buysorted[index][1]
+                if int(best_ask) <= acceptable_price - buy_buffer:  # buying
+                    print("BUY", str(-best_ask_amount) + "x", best_ask)
+                    orders.append(Order(product, best_ask, -best_ask_amount))
+                else:
+                    break
+
+        sell_buffer = 1
+        if len(order_depth.buy_orders) != 0:
+            for index in range(len(sellsorted)):
+                best_bid, best_bid_amount = sellsorted[index][0], sellsorted[index][1]
+                if int(best_bid) >= acceptable_price + sell_buffer:  # shorting
+                    print("SELL", str(best_bid_amount) + "x", best_bid)
+                    orders.append(Order(product, best_bid, -best_bid_amount))
+                else:
+                    break
+
+        result[product] = orders
+
     def run(self, state: TradingState):
         """Only method required. It takes all buy and sell orders for all
         symbols as an input, and outputs a list of orders to be sent."""
@@ -60,44 +96,18 @@ class Trader:
         storage = jsonpickle.decode(state.traderData) if state.traderData else {"EMERALDS": dict(),
                                                                               "TOMATOES": dict()}
         for product in state.order_depths:
-            order_depth: OrderDepth = state.order_depths[product]
-            buysorted = sorted(order_depth.sell_orders.items(), key=lambda x: x[0])
-            sellsorted = sorted(order_depth.buy_orders.items(), key=lambda x: x[0], reverse=True)
-            mid = self.compute_mid(storage, buysorted, sellsorted, state.timestamp)
-            storage[product][int(state.timestamp)] = mid
+            if product == 'TOMATOES':
+                order_depth: OrderDepth = state.order_depths[product]
+                buysorted = sorted(order_depth.sell_orders.items(), key=lambda x: x[0])
+                sellsorted = sorted(order_depth.buy_orders.items(), key=lambda x: x[0], reverse=True)
+                mid = self.compute_mid(storage, buysorted, sellsorted, state.timestamp)
+                storage[product][int(state.timestamp)] = mid
 
-            if len(storage[product]) < 2:
-                break
+                if len(storage[product]) < 2:
+                    break
 
-            if len(storage[product]) > 25:
-                storage[product].pop(next(iter(storage[product])))
-            intercept, slope = self.regression(product, storage)
-            prediction = slope*int(state.timestamp) + intercept
+                self.trade_tomatoes(state, storage, result, order_depth, buysorted, sellsorted)
 
-            orders: List[Order] = []  # Order(symbol, price, quantity)
-            acceptable_price = prediction  # Participant should calculate this value
-
-            buy_buffer = 1
-            if len(order_depth.sell_orders) != 0:
-                for index in range(len(buysorted)):
-                    best_ask, best_ask_amount = buysorted[index][0], buysorted[index][1]
-                    if int(best_ask) <= acceptable_price - buy_buffer:  # buying
-                        print("BUY", str(-best_ask_amount) + "x", best_ask)
-                        orders.append(Order(product, best_ask, -best_ask_amount))
-                    else:
-                        break
-
-            sell_buffer = 1
-            if len(order_depth.buy_orders) != 0:
-                for index in range(len(sellsorted)):
-                    best_bid, best_bid_amount = sellsorted[index][0], sellsorted[index][1]
-                    if int(best_bid) >= acceptable_price + sell_buffer:  # shorting
-                        print("SELL", str(best_bid_amount) + "x", best_bid)
-                        orders.append(Order(product, best_bid, -best_bid_amount))
-                    else:
-                        break
-
-            result[product] = orders
         traderData = jsonpickle.encode(storage)
         # No state needed - we check position directly
         conversions = 0
